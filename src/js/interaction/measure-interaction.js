0695a0308fdaa92c4b46c9ef40e752004cfb5211
Philippe Prévautel <philippe.prevautel@ign.fr>
2016-07-22 15:50:52 +0200

    ajout du less pour les outils de mesures

--- src/js/interaction/measure-interaction.js
+++ src/js/interaction/measure-interaction.js
@@ -24,13 +24,29 @@ var ol = ol || {};
 ol.interaction.Measure = function(options)	{
 	if (!options) options = {};
 	
-	var self_	= this;
-	this.listener	= null;
+    var self_           = this;
+	this.listener       = null;
 	this.measureOverlay = null;
-	this.id = generateUid();
 	this.multiple = options.multiple || false;
 	this.wgs84Sphere = new ol.Sphere(6378137);
+	this.currentFeature = null;
 
+    var authorizedTypes = new Array(
+        ol.geom.GeometryType.LINE_STRING,
+        ol.geom.GeometryType.POLYGON,
+        ol.geom.GeometryType.CIRCLE
+    );
+    
+    /**
+     * Type is authorized ?
+     * @param {ol.geom.GeometryType} type
+     * @returns {Boolean}
+     */
+    function isAuthorized(type) {
+        var index = authorizedTypes.indexOf(type);
+        return (index !== -1);
+    }
+    
     /**
      * interaction default style
      * @type ol.style.Style
@@ -44,7 +60,7 @@ ol.interaction.Measure = function(options)	{
 			width: 2
 		}),
 		image: new ol.style.Circle({
-			radius: 5,
+			radius: 3,
 			stroke: new ol.style.Stroke({
 				color: '#ffa500'
 			}),
@@ -55,69 +71,57 @@ ol.interaction.Measure = function(options)	{
 	});
 	
 	/**
-     * Layer default style
-     * @type ol.style.Style
+     * 
+     * @param {ol.Feature} feature
+     * @param {numeric} resolution
+     * @returns {Array}
      */
-	var defDrawStyle = new ol.style.Style({
-		fill: new ol.style.Fill({
-			color: 'rgba(255, 165, 0, 0.2)'
-		}),
-		stroke: new ol.style.Stroke({
-			color: '#ffcc33',
-			width: 2
-		}),
-		image: new ol.style.Circle({
-			radius: 7,
+	function styleFunction(feature, resolution) {
+		var style = new ol.style.Style({
 			fill: new ol.style.Fill({
-				color:  '#ffcc33'
+				color: 'rgba(255, 165, 0, 0.2)'
+			}),
+			stroke: new ol.style.Stroke({
+				color: '#ffa500',
+				width: 2
+			}),
+			text: new ol.style.Text({
+				font: 'bold 16px Arial, Verdana, Helvetica, sans-serif',
+				text:feature.get('measure'),
+				stroke: new ol.style.Stroke({
+					color: '#fff',
+					width: 5
+				})
 			})
-		})
-	});
-	
-	var drawStyle = options.drawStyle || defDrawStyle;
+		});
+		return [style];
+	}
+
 	this.layer =  new ol.layer.Vector({ 
 		source: new ol.source.Vector(),
-		style: drawStyle
+		style: styleFunction
 	});
 	
 	if (! options.style) {
 		options.style = defStyle;
 	}
-	
+    options.source = this.layer.getSource();
+    
 	/*if (options.type == ol.geom.GeometryType.LINE_STRING)	{
 		options.maxPoints = 2;
 	}*/
-	options.source = this.layer.getSource();
+    if (! options.type) {
+        options.type = ol.geom.GeometryType.LINE_STRING;
+    } else if (! isAuthorized(options.type)) {
+        throw 'Type ' + options.type + ' is not authorized.'; 
+    }
 	
 	ol.interaction.Draw.call(this, options);
-	
-	/**
-     * Format area output.
-     * @param {ol.geom.Polygon} polygon The polygon.
-     * @return {string} Formatted area.
-     */	
-	this.formatArea = function(polygon) {
-		var area;
-        
-        var sourceProj = this.getMap().getView().getProjection();
-        var geom = polygon.clone().transform(sourceProj, 'EPSG:4326');
-		
-		var coordinates = geom.getLinearRing(0).getCoordinates();
-		area = Math.abs(this.wgs84Sphere.geodesicArea(coordinates));
-       
-        var output;
-        if (area > 10000) {
-			output = (Math.round(area / 1000000 * 100) / 100) + ' km<sup>2</sup>';
-        } else {
-			output = (Math.round(area * 100) / 100) + ' m<sup>2</sup>';
-        }
-        return output;
-	};
-	
-	/**
-	 * Format length output.
-     * @param {ol.geom.LineString} line The line.
-	 * @return {string} The formatted length.
+
+    /**
+     * Format length ouput
+     * @param {ol.geom.LineString} line
+     * @return {object} The formatted length.
      */
 	this.formatLength = function(line) {
         var length = 0;
@@ -130,31 +134,80 @@ ol.interaction.Measure = function(options)	{
 			length += this.wgs84Sphere.haversineDistance(c1, c2);
 		}
         
-        var output;
+        var output = {};
         if (length > 100) {
-			output = (Math.round(length / 1000 * 100) / 100) + ' km';
+			output.html = output.measure = (Math.round(length / 1000 * 100) / 100) + ' km';
         } else {
-			output = (Math.round(length * 100) / 100) + ' m';
+			output.html = output.measure = (Math.round(length * 100) / 100) + ' m';
         }
 		return output;
 	};
-	  
+	
+    /**
+     * Format area output.
+     * @param {ol.geom.Polygon} polygon
+     * @return {object} Formatted area.
+     */
+	this.formatArea = function(polygon) {
+		var area;
+        
+        var sourceProj = this.getMap().getView().getProjection();
+        var geom = polygon.clone().transform(sourceProj, 'EPSG:4326');
+		
+		var coordinates = geom.getLinearRing(0).getCoordinates();
+		area = Math.abs(this.wgs84Sphere.geodesicArea(coordinates));
+       
+        var output = {};
+        if (area > 10000) {
+			area = (Math.round(area / 1000000 * 100) / 100);
+			output.html = area + ' km<sup>2</sup>';
+			output.measure = area + ' km2';
+        } else {
+			area =(Math.round(area * 100) / 100);
+			output.html = area + ' m<sup>2</sup>';
+			output.measure = area + ' m2';
+        }
+        return output;
+	};
+    
+    /**
+     * Format area output for a circle.
+     * @param {ol.geom.Circle} circle
+     * @return {object} Formatted area.
+     */
+	this.formatCircleArea = function(circle) {
+		var sourceProj = this.getMap().getView().getProjection();
+        var geom = circle.clone().transform(sourceProj, 'EPSG:4326');
+        
+		var radius = this.wgs84Sphere.haversineDistance(
+            geom.getFirstCoordinate(),
+            geom.getLastCoordinate()
+        );
+        var area = Math.PI * Math.pow(radius, 2);
+      
+		var output = {};
+        if (area > 10000) {
+			area = (Math.round(area / 1000000 * 100) / 100);
+			output.html = area + ' km<sup>2</sup>';
+			output.measure = area + ' km2';
+        } else {
+			area =(Math.round(area * 100) / 100);
+			output.html = area + ' m<sup>2</sup>';
+			output.measure = area + ' m2';
+        }
+        return output;
+	};
+    
     /**
      * Tooltip overlay creation
      */
-	this.createMeasureTooltip = function() {
-		var elt = document.createElement('div');
-		elt.className = 'tooltip-' + this.id + ' tooltip-measure';
-		
-		this.measureOverlay = new ol.Overlay({
-			element: elt,
-			offset: [0, -15],
-			positioning: 'bottom-center'
-		});
-		if (this.getMap()) {
-			this.getMap().addOverlay(this.measureOverlay);
-		}
-	};
+	var elt = document.createElement('div');
+	elt.className = 'tooltip-' + generateUid() + ' tooltip-measure';
+    this.measureOverlay = new ol.Overlay({
+       element: elt,
+       offset: [0, -15],
+       positioning: 'bottom-center'
+    });
 	
     /**
      * Manage drawstart event
@@ -162,27 +215,36 @@ ol.interaction.Measure = function(options)	{
 	this.on('drawstart', function(evt) {
 		if (! this.multiple) {
 			self_.layer.getSource().clear();
-			if (self_.measureOverlay) {
-				var elt = self_.measureOverlay.getElement();
-				if (elt.parentNode) { elt.parentNode.removeChild(elt); }
-			}
 		}
-		this.createMeasureTooltip();
 		
 		var ttPos;
+		self_.currentFeature = evt.feature;
+		
+		/**
+		 * Listener on geometry change
+		 */
 		self_.listener = evt.feature.getGeometry().on('change', function(evt) {
+			if (self_.measureOverlay.getElement().style.visibility === 'hidden') {
+				self_.measureOverlay.getElement().style.visibility = 'visible';
+			}
+	
 			var geom = evt.target;
 			
 			var output;
-			if (geom instanceof ol.geom.Polygon) {
-				output = self_.formatArea(geom);
-				ttPos = geom.getInteriorPoint().getCoordinates();
-			} else if (geom instanceof ol.geom.LineString) {
+            if (geom instanceof ol.geom.LineString) {
 				output = self_.formatLength(geom);
 				ttPos = geom.getLastCoordinate();
+			} else if (geom instanceof ol.geom.Polygon) {
+				output = self_.formatArea(geom);
+				ttPos = geom.getLastCoordinate();
+			} else if (geom instanceof ol.geom.Circle) {
+				output = self_.formatCircleArea(geom);
+				ttPos = geom.getCenter();
 			}
+			
 			var elt = self_.measureOverlay.getElement();
-			elt.innerHTML = output;
+			elt.innerHTML = output.html;
+			self_.currentFeature.set('measure', output.measure);
 			self_.measureOverlay.setPosition(ttPos);	
 		});
 	}, this);
@@ -191,10 +253,7 @@ ol.interaction.Measure = function(options)	{
      * Manage drawend event
      */
 	this.on('drawend', function(evt) {
-		var elt = self_.measureOverlay.getElement();
-		elt.className =  'tooltip-' + this.id + ' tooltip-static';
-		
-		self_.measureOverlay.setOffset([0, -7]);
+		self_.measureOverlay.getElement().style.visibility = 'hidden';
 		ol.Observable.unByKey(self_.listener);
 	}, this);
 };
@@ -210,8 +269,11 @@ ol.interaction.Measure.prototype.setMap = function (map) {
 
 	if (this.getMap())	{
 		this.getMap().removeLayer(this.layer);
+		this.getMap().removeOverlay(this.measureOverlay);
     }
+
 	map.addLayer(this.layer);
+	map.addOverlay(this.measureOverlay);
 };
 
 /**
@@ -221,12 +283,4 @@ ol.interaction.Measure.prototype.setMap = function (map) {
 ol.interaction.Measure.prototype.setActive = function(active)	{
 	ol.interaction.Draw.prototype.setActive.call(this, active);
 	this.layer.getSource().clear();
-	
-	if (! active) {
-		var selector = "div[class^='tooltip-" + this.id + "']";
-		var nodes = document.querySelectorAll(selector);
-		for (var n=0; n<nodes.length; ++n) {
-			nodes[n].parentNode.removeChild(nodes[n]);
-		}
-	}
 };
