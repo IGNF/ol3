8116e3761850f7faadb0307b9ab28666dc2d4b62
Philippe Prévautel <philippe.prevautel@ign.fr>
2016-08-31 14:27:46 +0200

    bricole

---
+++ src/js/Utils.js
@@ -0,0 +1,8 @@
+/**
+ * Returns france center in WGS84 Web Mercator (EPSG:3857)
+ * @returns {ol.Coordinate}
+ */
+ol.utils.getFranceCenter = function() {
+    return [ 251936.04517352092, 5916836.764449345 ];
+};
+
