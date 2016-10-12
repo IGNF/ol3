09a945b477eb7ba8f5dcb0c05213a7de4826d9cf
Philippe Prévautel <philippe.prevautel@ign.fr>
2016-10-12 10:27:00 +0200

    Mise en forme de la fiche d'un feature

--- src/js/Utils.js
+++ src/js/Utils.js
@@ -5,19 +5,3 @@
 ol.utils.getFranceCenter = function() {
     return [ 251936.04517352092, 5916836.764449345 ];
 };
-
-/**
- * Returns unique identifier
- * @returns {String}
- */
-ol.utils.guid = function() {
-    function s4() {
-        return Math.floor((1 + Math.random()) * 0x10000)
-            .toString(16)
-            .substring(1);
-    };
-
-    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-        s4() + '-' + s4() + s4() + s4();
-};
-
