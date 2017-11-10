bb22ef94e043362eabb889be83f7f04a87750466
Philippe Prévautel <philippe.prevautel@ign.fr>
2017-11-10 09:29:52 +0100

    ajout des resolutions suivant les niveaux de zoom

--- src/js/Utils.js
+++ src/js/Utils.js
@@ -5,3 +5,26 @@
 ol.utils.getFranceCenter = function() {
     return [ 251936.04517352092, 5916836.764449345 ];
 };
+
+
+/**
+ * Returns resolution for zoom level
+ * @returns {ol.utils.resolutions}
+ */
+ol.utils.resolutions = function() {
+    var res0 = 156543.03392804097;
+    
+    var resolutions = {"0": res0};
+    for (var i=1; i<=20; ++i) {
+        var resolution = res0 / Math.pow(2, i) ;
+        resolutions[i.toString()] = resolution;
+    }
+    
+    this.getResolution = function(zoomLevel) {
+        var z = zoomLevel.toString();
+        if (z in resolutions) {
+            return resolutions[z];
+        }
+        return null;
+    };
+};
